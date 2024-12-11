aef639cb4e808341cb73d7291b110cea2d677b9e
shashank <shashanknegi@pearlorganisation.com>
2024-12-11 13:43:29 +0530

    added changes

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { checkStatus, orderPayment} from "../controller/payment.js";
+import { checkStatus, getPaymentById, getPayments, orderPayment} from "../controller/payment.js";
 import { verifyTokenMiddleware } from "../middleware/verifyTokenMiddleware.js";
 
 const router = express.Router();
@@ -10,6 +10,8 @@ router.route('/order')
 // router.route('/status:transactionId')
 // .post(verifyTokenMiddleware ,paymentStatus);
 router.post('/status/:txnId', checkStatus);
+router.get('/payment-histories', getPayments);
+router.get('/payment-histories/:id', getPaymentById);
 
 
 
