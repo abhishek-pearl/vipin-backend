ea1fcfdc1f4ee72a523077943e09043c317f93fa
shashank <shashanknegi@pearlorganisation.com>
2025-02-19 17:00:42 +0530

    added changes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { forgetPassword, login, logout, refreshToken, signup } from "../controller/auth.js";
+import { forgetPassword, login, logout, refreshToken, signup, verifyForgetPassword } from "../controller/auth.js";
 
 const authRouter = express.Router();
 
@@ -8,5 +8,6 @@ authRouter.route("/signout").post(logout);
 authRouter.route("/refresh").post(refreshToken);
 authRouter.route("/signup").post(signup);
 authRouter.route('/forgetPassword').post(forgetPassword)
+authRouter.route('/verifyForgetPassword/:token').post(verifyForgetPassword)
 
 export default authRouter;
